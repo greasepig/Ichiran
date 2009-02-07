2dc37a103f3a62dbdf8997dce141ebfbdcbdcd23
Moss Gross <greasepig@gmail.com>
2009-02-07 19:55:38 +0900

    asdf

--- config/routes.rb
+++ config/routes.rb
@@ -33,8 +33,7 @@ ActionController::Routing::Routes.draw do |map|
   #   end
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-#  map.root :controller => 'k', :action => :l
-  map.connect '', :controller => 'k' :action => :l
+  map.root :controller => 'k', :action => :l
 
   # See how all your routes lay out with "rake routes"
 
